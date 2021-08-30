a16502ec22439d972d09ffe080fdae20361d7d99
daniellerandall <dkrandall32@gmail.com>
2021-08-30 09:04:15 -0400

    Added transfer/openNewAccount/accountSummary/transactionHistory

--- atm/atm-console/src/main/java/com/smbcgroup/training/atm/console/ATM.java
+++ atm/atm-console/src/main/java/com/smbcgroup/training/atm/console/ATM.java
@@ -4,8 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Random;
 
 import com.smbcgroup.training.atm.dao.txtFile.AccountAccessor;
 
@@ -15,9 +20,9 @@ public class ATM {
 	}
 
 	private static enum Action {
-		login, changeAccount, checkBalance, deposit, withdraw;
+		login, changeAccount, checkBalance, deposit, withdraw, transfer, openNewAccount, accountsSummary, transactionHistory;
 		// TODO: add more actions
-		//added: deposit, withdraw
+		//added: deposit, withdraw, transfer, openNewAccount, accountsSummary, TransactionHistory
 	}
 	
 	private BufferedReader inputReader;
@@ -25,6 +30,9 @@ public class ATM {
 	private String[] loggedInUserAccounts;
 	private String selectedAccount;
 	private Action selectedAction = Action.login; //selectedAction is originally = login
+	private String loggedInUser; //added this
+	private ArrayList<String> transactionLogger = new ArrayList<String>();
+	
 
 	private ATM(InputStream input, PrintStream output) {
 		this.inputReader = new BufferedReader(new InputStreamReader(input));
@@ -79,12 +87,26 @@ public class ATM {
 		case withdraw:
 			output.println("Enter amount to withdraw");
 			return true;
+		//case for transfer
+		case transfer:
+			output.println("Enter account to transfer in amount in following format: account,amount");
+			return true;
+		//case for opening new acct
+		case openNewAccount:
+			output.println("Enter amount to deposit into new account.");
+			return true;
+		//case for getting account summary:
+		case accountsSummary:
+			return false;
+		//case for transaction history
+		case transactionHistory:
+			return false;
 		default: //case of checkBalance
 			return false;
 		}
 	}
 
-	private Action performActionAndGetNextAction(String input) throws ATMException, SystemExit {
+	private Action performActionAndGetNextAction(String input) throws ATMException, SystemExit, IOException {
 		if ("exit".equals(input))
 			throw new SystemExit();
 		if (selectedAction == null) {
@@ -98,6 +120,7 @@ public class ATM {
 		case login:
 			try {
 				loggedInUserAccounts = AccountAccessor.getUserAccounts(input);
+				loggedInUser = input;
 				return Action.changeAccount;
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -140,6 +163,7 @@ public class ATM {
 				
 				//print message saying deposit successful
 				output.println("Deposit successful. Updated balance: $" + newBalance);
+				transactionLogger.add("Deposit of $" + depositAmount + " made to account " + selectedAccount);
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new ATMException("Invalid amount to deposit.");
@@ -153,14 +177,52 @@ public class ATM {
 				BigDecimal newBalance = origBalance.subtract(withdrawAmount);
 				AccountAccessor.updateAccountBalance(selectedAccount, newBalance);
 				output.println("Withdraw successful. Updated balance: $" + newBalance);
+				transactionLogger.add("Withdraw of $" + withdrawAmount + " made from account " + selectedAccount);
 				
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new ATMException("Invalid amount to withdraw.");
 			}
 			break;
-			
-			
+		case transfer:
+			try {
+				String[] transferInputs = input.split(",");
+				String transferAccount = transferInputs[0];
+				BigDecimal transferAmount = new BigDecimal(transferInputs[1]);
+				BigDecimal transferAcctOrigBalance = AccountAccessor.getAccountBalance(transferAccount);
+				BigDecimal AcctOrigBalance = AccountAccessor.getAccountBalance(selectedAccount);
+				AccountAccessor.updateAccountBalance(transferAccount, transferAmount.add(transferAcctOrigBalance));
+				AccountAccessor.updateAccountBalance(selectedAccount, AcctOrigBalance.subtract(transferAmount));
+				output.println("Tranfer of $" + transferAmount + " from account " + selectedAccount + " to  account " + transferAccount + " successful.");
+				output.println("New balance of account " + selectedAccount + ": " + AccountAccessor.getAccountBalance(selectedAccount));
+				output.println("New balance of account " + transferAccount + ": " + AccountAccessor.getAccountBalance(transferAccount));
+				transactionLogger.add("Transfer of $" + transferAmount + " made from account " + selectedAccount + " to account " + transferAccount);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new ATMException("An error occured. Please try again");
+			}
+			break;
+		case openNewAccount:
+			//generate a new random account number
+			Random randomAccountGenerator = new Random();
+			int newAcctNumber = randomAccountGenerator.nextInt(999999);
+			AccountAccessor.createNewAccount(String.valueOf(newAcctNumber), input);
+			AccountAccessor.addNewAccountToUserID(loggedInUser, String.valueOf(newAcctNumber));
+			loggedInUserAccounts = AccountAccessor.getUserAccounts(loggedInUser);
+			System.out.println("New account numbered " + newAcctNumber + " created with opening balance of $" + input + ".");
+			break;
+		case accountsSummary:
+			System.out.println(loggedInUser + "'s accounts: ");
+			for(String userAccount : loggedInUserAccounts) {
+				System.out.println("Account: " + userAccount + "; balance: " + AccountAccessor.getAccountBalance(userAccount));
+			}
+			break;
+		case transactionHistory:
+			System.out.println(loggedInUser + "'s transaction history:");
+			for (String transaction : transactionLogger) {
+				System.out.println(transaction);
+			}
+			break;
 		}
 		return null;
 	}

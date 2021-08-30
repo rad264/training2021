da0aaf3ad1092e658bd38b43b6e688d0d0235112
daniellerandall <dkrandall32@gmail.com>
2021-08-30 11:37:53 -0400

    Used builder patter

--- common-patterns/src/main/java/com/smbcgroup/training/builder/ImmutableDate.java
+++ common-patterns/src/main/java/com/smbcgroup/training/builder/ImmutableDate.java
@@ -1,7 +1,69 @@
 package com.smbcgroup.training.builder;
 
 public class ImmutableDate {
-
+	
+	public static class Builder {
+		private int year;
+		private int month;
+		private int day;
+		private int hour;
+		private int minute;
+		private int second;
+		private int millisecond;
+		
+		public Builder withYear(int year) {
+			this.year = year;
+			return this; //return the builder
+		}
+		
+		
+		public Builder withMonth(int month) {
+			this.month = month;
+			return this; 
+		}
+		
+		public Builder withDay(int day) {
+			this.day = day;
+			return this;
+		}
+		
+		public Builder withHour(int hour) {
+			this.hour = hour;
+			return this;
+		}
+		
+		public Builder withMinute(int minute) {
+			this.minute = minute;
+			return this;
+		}
+		
+		public Builder withSecond(int second) {
+			this.second = second;
+			return this;
+		}
+		
+		public Builder withMillisecond(int millisecond) {
+			this.millisecond = millisecond;
+			return this;
+		}
+		
+		public ImmutableDate build() {
+			ImmutableDate date = new ImmutableDate();
+			date.year = this.year;
+			date.month = this.month;
+			date.day = this.day;
+			date.hour = this.hour;
+			date.minute = this.minute;
+			date.second = this.second;
+			date.millisecond = this.millisecond;
+			
+			return date;
+			
+		}
+		
+		
+	}
+	
 	private int year = 1979;
 	private int month;
 	private int day = 1;
@@ -9,8 +71,10 @@ public class ImmutableDate {
 	private int minute;
 	private int second;
 	private int millisecond;
+	
 
-	public ImmutableDate(int year, int month, int day, int hour, int minute, int second, int millisecond) {
+	/*
+	 * public ImmutableDate(int year, int month, int day, int hour, int minute, int second, int millisecond) {
 		this.year = year;
 		this.month = month;
 		this.day = day;
@@ -19,10 +83,40 @@ public class ImmutableDate {
 		this.second = second;
 		this.millisecond = millisecond;
 	}
+	 */
+	private ImmutableDate() {
+	
+	}
 
 	@Override
 	public String toString() {
 		return String.format("%d-%d-%d %d:%d:%d:%d", year, month, day, hour, minute, second, millisecond);
 	}
 
+	public static void main(String[] args) {
+		ImmutableDate date1 = new ImmutableDate();
+		System.out.println(date1);
+		
+		ImmutableDate date2 = new ImmutableDate.Builder()
+				.withYear(2021)
+				.withMonth(8)
+				.withDay(27)
+				.withHour(5)
+				.withMinute(29)
+				.withSecond(47)
+				.withMillisecond(10)
+				.build();
+		
+		System.out.println(date2);
+		
+		ImmutableDate date3 = new ImmutableDate.Builder()
+				.withYear(2021)
+				.withMonth(8)
+				.withHour(5)
+				.build();
+		
+		System.out.println(date3);
+		
+	}
+	
 }
--- common-patterns/src/test/java/com/smbcgroup/training/builder/ImmutableDateTest.java
+++ common-patterns/src/test/java/com/smbcgroup/training/builder/ImmutableDateTest.java
@@ -8,12 +8,26 @@ public class ImmutableDateTest {
 
 	@Test
 	public void test2021_8_26_16_45_59_999() {
-		assertEquals("2021-8-26 16:45:59:999", new ImmutableDate(2021, 8, 26, 16, 45, 59, 999).toString());
+	ImmutableDate date = new ImmutableDate.Builder()
+			.withYear(2021)
+			.withMonth(8)
+			.withDay(26)
+			.withHour(16)
+			.withMinute(45)
+			.withSecond(59)
+			.withMillisecond(999)
+			.build();
+	assertEquals("2021-8-26 16:45:59:999", date.toString());
 	}
 
 	@Test
 	public void test2021_8_26_00_00_00_000() {
-		assertEquals("2021-8-26 0:0:0:0", new ImmutableDate(2021, 8, 26, 0, 0, 0, 0).toString());
+		ImmutableDate date = new ImmutableDate.Builder()
+				.withYear(2021)
+				.withMonth(8)
+				.withDay(26)
+				.build();
+	assertEquals("2021-8-26 0:0:0:0", date.toString());
 	}
 
 }
